56bfdbedeaea22ba3ff3acc362485f51084a263d
sandeshnilas <sandeshnilaskhatwada@gmail.com>
2024-08-24 22:07:27 +0545

    "feat: create user using auth0"

--- MERN/backend/src/index.ts
+++ MERN/backend/src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 
+import MyUserRoutes from "./routes/MyUserRoutes"
+
 mongoose.connect(process.env.MONGO_CONNECTION_STRING as string).then(()=>{
     console.log("Connected to the database")
 })
@@ -12,9 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
-    res.json({ message: "Hello" });
-});
+app.use("/api/my/user", MyUserRoutes)
 
 app.listen(7000, () => {
     console.log("server started on port 7000");
