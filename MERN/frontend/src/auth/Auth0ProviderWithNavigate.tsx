8c384590db19108400572424c5ccb10628356a00
SandeshNilasKhatiwada <149852523+SandeshNilasKhatiwada@users.noreply.github.com>
2024-08-26 12:57:25 +0545

    "chore: backend and frontend user validation check" (#2)

--- MERN/frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ MERN/frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,29 +1,28 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+
+import { Auth0Provider} from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
     children: React.ReactNode;
 }
 
 function Auth0ProviderWithNavigate({children}:Props) {
-
-    const {createUser} = useCreateMyUser();
+    const navigate = useNavigate()
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+    const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-    if (!domain || !clientId || !redirectUri){
+    if (!domain || !clientId || !redirectUri || !audience){
         throw new Error ("unable to initalise auth");
     }
-    const onRedirectCallback = (appState?: AppState, user?: User) =>{
-        if(user?.sub && user?.email){
-            createUser ({auth0Id: user.sub, email: user.email})
-        }
+    const onRedirectCallback = async () =>{
+        navigate("/auth-callback")
 
 
     }
     return (
-       <Auth0Provider domain={domain} clientId={clientId} authorizationParams={{redirect_uri: redirectUri}} onRedirectCallback={onRedirectCallback}>
+       <Auth0Provider domain={domain} clientId={clientId} authorizationParams={{redirect_uri: redirectUri, audience}} onRedirectCallback={onRedirectCallback}>
         {children}
        </Auth0Provider>
     )
